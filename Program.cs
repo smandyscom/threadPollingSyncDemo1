b23f6050e14673d1db51348dd1787f76d76cc470
smandyscom <smandyscom@gmail.com>
2017-07-18 22:24:40 +0800

    initialized

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace thread_demo
+{
+    class Program
+    {
+
+        static Func<int> __delegate =  new Func<int>(method);
+        static void Main(string[] args)
+        {
+            var handle = __delegate.BeginInvoke((IAsyncResult ar)=> 
+            {
+                method();
+                __delegate.EndInvoke(ar);
+            },null);
+
+            while (!handle.IsCompleted)
+            {
+                
+            }
+
+            Console.WriteLine("");
+        }
+
+        static int method()
+        {
+            //reflect the current thread
+            Console.WriteLine(System.Threading.Thread.CurrentThread.Name);
+            return 0;
+        }
+
+    }
+}
